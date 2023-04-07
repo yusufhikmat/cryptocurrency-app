048acaca5394d081ad57292c2588b450c30aaaba
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-04-06 23:49:00 -1200

    created navbar.js and footer folder

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button,Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
